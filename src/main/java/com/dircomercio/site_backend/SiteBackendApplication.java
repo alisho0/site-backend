fd02b2a7c325b6876687759153d9084ebbd25939
alisho0 <alejorea2015@gmail.com>
2025-04-15 22:05:50 -0300

    Creación de la entidad Persona para que se vayan guiando. Yo usé los annotations de lombok para el Data y los otros. En Notion les explico más en detalle. Faltan las relaciones, pero las pondré con el tiempo

---
+++ src/main/java/com/dircomercio/site_backend/SiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dircomercio.site_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteBackendApplication.class, args);
+	}
+
+}
