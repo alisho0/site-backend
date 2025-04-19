55eb9957381767ee362b12686bc286eca9374972
alisho0 <alejorea2015@gmail.com>
2025-04-19 00:03:13 -0300

    Corrección al código de Maxi

--- src/main/java/com/dircomercio/site_backend/entities/Pase.java
+++ src/main/java/com/dircomercio/site_backend/entities/Pase.java
@@ -1,29 +1,31 @@
 package com.dircomercio.site_backend.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persitence.GeneratedValue;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import jakarta.AllArgsConstructor;
+import jakarta.annotation.Generated;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate; 
 
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "pases")
+@Data
 public class Pase {
-
+    
+    
     @Id
-    @NoArgsConstructor
-    @AllArgsConstructor
-    @Entity
-    @Table(name = "pases")
-    @Data
-
-    private Long idPase;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
     private String accion;
     
@@ -35,9 +37,9 @@ public class Pase {
 
     private String descripcion;
 
-    @ManyToOne
-    @JoinColumn(name = "expediente_id", nullable=false)
-    private Expediente expediente;
+    // @ManyToOne
+    // @JoinColumn(name = "expediente_id", nullable=false)
+    // private Expediente expediente;
 
 }
 
