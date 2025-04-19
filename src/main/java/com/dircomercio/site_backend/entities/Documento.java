0611bb96e7a4626d4f0c9ae5c5f105a955334061
alisho0 <alejorea2015@gmail.com>
2025-04-18 23:33:24 -0300

    Cambié los imports

--- src/main/java/com/dircomercio/site_backend/entities/Documento.java
+++ src/main/java/com/dircomercio/site_backend/entities/Documento.java
@@ -8,6 +8,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -16,7 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Entity
 @Table(name = "documentos")
-// @Builder
+@Builder
 public class Documento {
 
     @Id
