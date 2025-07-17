4d981a28b3d4fb5459aafe18abe2da81780a4b75
alisho0 <alejorea2015@gmail.com>
2025-07-16 21:44:21 -0300

    Cambios de cors para probar

--- src/main/java/com/dircomercio/site_backend/auth/config/CorsConfig.java
+++ src/main/java/com/dircomercio/site_backend/auth/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                    .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175")
+                    .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "https://site-backend-f8xg.onrender.com")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                     .allowedHeaders("*")
                     .allowCredentials(true);
