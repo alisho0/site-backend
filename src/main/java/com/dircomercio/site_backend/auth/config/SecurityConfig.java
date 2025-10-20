d1aec93043b23f65e112125a3aef2952ba053ac9
Alejandro <alejorea2015@gmail.com>
2025-10-20 09:47:18 -0300

    Cambios para el servidor

--- src/main/java/com/dircomercio/site_backend/auth/config/SecurityConfig.java
+++ src/main/java/com/dircomercio/site_backend/auth/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:8080"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174", "http://localhost:5175", "http://localhost:8080", "100.83.50.21:8080", "http://100.83.50.21:8080"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
@@ -69,7 +69,6 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.GET, "/usuarios/{id}").hasAnyRole("ADMIN", "DIRECCION") // Ver detalles
                 .requestMatchers(HttpMethod.PUT, "/usuarios/{id}").hasAnyRole("ADMIN", "DIRECCION") // Editar
                 .requestMatchers(HttpMethod.DELETE, "/usuarios/borrar/**").hasAnyRole("ADMIN", "DIRECCION") // Borrar
-                .requestMatchers(HttpMethod.POST, "/auth/register").hasAnyRole("ADMIN", "DIRECCION") // Crear
 
                 // Reglas para Expedientes, Pases, etc.
                 .requestMatchers("/expediente/traerPorUsuario", "/expediente/traerExpedientePorId/{id}", "/pases/**", "/pases/traerPasesPorExp/{id}", "/audiencias/**", "/doc/traerOrdenesPorExpediente/{expedienteId}", "/doc/eliminarDoc/{id}", "/doc/crearOrden").hasAnyRole("ADMIN", "ABOGADO", "DIRECCION")
